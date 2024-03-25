3329cb9f211ec9545075f8d49cde254ba696bf69
dperezg2021 <159420331+dperezg2021@users.noreply.github.com>
2024-03-25 14:29:42 +0100

    commit rauw

--- P3/fisica.js
+++ P3/fisica.js
@@ -1,6 +1,11 @@
-console.log("Ejecutando JS...");
-
 const canvas = document.getElementById("canvas");
+const timerDisplay = document.getElementById("timer");
+const resultDisplay = document.getElementById("result");
+const angleInput = document.getElementById("angle");
+const velocityInput = document.getElementById("velocity");
+const fireButton = document.getElementById("fire");
+const resetButton = document.getElementById("reset");
+const AnguloValor = document.getElementById("valorangulo");
 
 //-- Definir el tamaño del canvas
 canvas.width = 900;
@@ -10,72 +15,207 @@ canvas.height = 400;
 const ctx = canvas.getContext("2d");
 
 //-- Coordenadas del objeto
+let xposicion = 20;
+let yposicion = canvas.height - 50; // Ajustamos la posición inicial del cuadrado más arriba
 let x = 0;
-let y = 10;
+let y = 0;
+let velx = 0;
+let vely = 0;
+let flightTime = 0;
+let timerInterval;
+let initialVelocity = 10;
+let launchAngle = 0;
+const gravity = 0.3;
+var colorMarronOscuro = '#654321';
+var nuevaPosicionX = generarPosicionXAleatoria();
 
-//-- Velocidad horizontal del objeto
-let velx = 3;
-let vely = 1;
+
+angleInput.addEventListener('click', () => {
+  AnguloValor.textContent = angleInput.value;
+
+});
+
+
+//-- Función para iniciar el contador de tiempo
+function startTimer() {
+  flightTime = 0;
+  timerInterval = setInterval(() => {
+      flightTime++;
+      timerDisplay.textContent = flightTime;
+  }, 1000);
+}
+
+//-- Función para detener el contador de tiempo
+function stopTimer() {
+  clearInterval(timerInterval);
+}
+
+function generarPosicionXAleatoria() {
+  // Genera un número aleatorio entre 0 y el ancho total del lienzo (canvas.width)
+  return Math.floor(Math.random() * canvas.width);
+}
+//-- Función para reiniciar la animación
+function resetAnimation() {
+  xposicion = 10; // Restaurado a la esquina inferior izquierda
+  yposicion = canvas.height - 50; // Restaurado a la esquina inferior izquierda
+  flightTime = 0;
+  timerDisplay.textContent = flightTime;
+  resultDisplay.textContent = '-';
+  stopTimer();
+  update();
+}
+
+//-- Evento para el botón de disparo
+fireButton.addEventListener('click', () => {
+  launchAngle = parseFloat(angleInput.value);
+  initialVelocity = parseFloat(velocityInput.value);
+  velx = initialVelocity * Math.cos((launchAngle * Math.PI) / 180)
+  console.log(initialVelocity);
+  console.log(launchAngle);
+  vely = initialVelocity * Math.cos((launchAngle * Math.PI) / 180)
+  startTimer(); // Inicia el contador de tiempo
+});
+
+//-- Evento para el botón de inicio
+resetButton.addEventListener('click', () => {
+  resetAnimation();
+  generarPosicionXAleatoria(); // Reinicia la animación
+});
+
+
+
 
 //-- Función principal de animación
-function update() 
-{
-  console.log("test");
-  //-- Algoritmo de animación:
-  //-- 1) Actualizar posiciones de los elementos
+function update() {
+    //-- Algoritmo de animación:
+    //-- 1) Actualizar posiciones de los elementos
 
-  //-- Comprobar colisión con borde derecho
-  //-- Si se alcanza la anchura del canvas, se cambia la velocidad
-  //-- de signo (rebote)
-  if (x < 0 || x >= (canvas.width - 20) ) {
-    velx = -velx;
-  }
-    //-- Condición de rebote en extremos horizontales del canvas
-    if (y <= 0 || y > 400) {
-        vely = -vely;
-    }
-    x = x + velx;
-    y = y + vely;
+    //-- 2) Borrar el canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  //-- 2) Borrar el canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //-- 3) Dibujar los elementos visibles
+     //-- Dibujar la diana
+    ctx.beginPath();
+    ctx.arc(nuevaPosicionX, canvas.height - 33, 32, 0, 2 * Math.PI); // Círculo exterior (rojo)
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+    ctx.fillStyle = 'red';
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
 
-  //-- 3) Dibujar los elementos visibles
-  ctx.beginPath();
-    ctx.rect(x, y, 40, 40);
+    ctx.beginPath();
+    ctx.arc(nuevaPosicionX, canvas.height - 33, 26, 0, 2 * Math.PI); // Círculo interior (blanco)
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 2;
+    ctx.fillStyle = 'white';
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
 
-    //-- Dibujar
-    ctx.fillStyle = 'green';
+    ctx.beginPath();
+    ctx.arc(nuevaPosicionX, canvas.height - 33, 20, 0, 2 * Math.PI); // Círculo exterior (rojo)
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+    ctx.fillStyle = 'red';
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
 
+    ctx.beginPath();
+    ctx.arc(nuevaPosicionX, canvas.height - 33, 14, 0, 2 * Math.PI); // Círculo interior (blanco)
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 2;
+    ctx.fillStyle = 'white';
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
+    
+    ctx.beginPath();
+    ctx.arc(nuevaPosicionX, canvas.height - 33, 8, 0, 2 * Math.PI); // Círculo exterior (rojo)
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+    ctx.fillStyle = 'red';
+    ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
 
+  //-- Dibujar la flecha 
+  //-- Dibujar cuerpo de la flecha
 
-    //-- Rellenar
+    ctx.beginPath();
+    ctx.moveTo(xposicion + 5, yposicion + 10);  // Punto superior derecho del cuerpo de la flecha
+    ctx.lineTo(xposicion + 15, yposicion + 20);  // Punto inferior derecho del cuerpo de la flecha
+    ctx.lineTo(xposicion + 5, yposicion + 30);  // Punto inferior izquierdo del cuerpo de la flecha
+    ctx.closePath();
+
+    //-- Rellenar la punta de la flecha
+    ctx.fillStyle = colorMarronOscuro; 
+    ;
     ctx.fill();
 
-    //-- Dibujar el trazo
-    ctx.stroke()
-  ctx.closePath();
+    ctx.beginPath();
+    ctx.rect(xposicion-3, yposicion+10, 8, 20); // Posición y tamaño del cuadrado
+    ctx.fillStyle = colorMarronOscuro;  // Color del cuadrado (puedes cambiarlo)
+    ctx.fill(); // Rellenar el cuadrado
+    ctx.closePath();
 
-  ctx.beginPath();
-    //-- Dibujar un circulo: coordenadas x,y del centro
-    //-- Radio, Angulo inicial y angulo final
-    ctx.arc(100, 350, 10, 0, 2 * Math.PI);
-    ctx.strokeStyle = 'blue';
+    ctx.beginPath();
+    ctx.moveTo(xposicion+3, yposicion + 20);  // Comienza desde el centro izquierdo
+    ctx.lineTo(xposicion + 35, yposicion + 20);  // Hasta el centro derecho
+    ctx.closePath();
+    ctx.strokeStyle = 'white';
     ctx.lineWidth = 5;
-    ctx.fillStyle = 'red';
+    ctx.stroke();
 
-    //-- Dibujar el trazo
-    ctx.stroke()
+    ctx.beginPath();
+    ctx.moveTo(xposicion +35, yposicion + 20);  // Comienza desde el centro izquierdo
+    ctx.lineTo(xposicion + 40, yposicion + 20);  // Hasta el centro derecho
+    ctx.closePath();
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 5;
+    ctx.stroke();
 
-    //-- Dibujar el relleno
-    ctx.fill()
-    
-ctx.closePath()
+    //-- Dibujar punta de la flecha
+    ctx.beginPath();
+    ctx.moveTo(xposicion + 40, yposicion + 10);  // Punto superior derecho del cuerpo de la flecha
+    ctx.lineTo(xposicion + 60, yposicion + 20);  // Punto inferior derecho del cuerpo de la flecha
+    ctx.lineTo(xposicion + 40, yposicion + 30);  // Punto inferior izquierdo del cuerpo de la flecha
+    ctx.closePath();
 
-  //-- 4) Volver a ejecutar update cuando toque
+    //-- Rellenar la punta de la flecha
+    ctx.fillStyle = 'black';
+    ctx.fill();
+
+    //-- 4) Actualizar las coordenadas del proyectil en movimiento parabólico
+    if (flightTime > 0) {
+        // Actualizar posición en función de la velocidad
+        xposicion += velx;
+        yposicion -= vely - 0.5 * gravity * flightTime * flightTime;
+        vely -= gravity * flightTime;
+
+        // Verificar los límites del canvas
+        if (xposicion < 0) {
+            xposicion = 0; // Evitar que el cuadrado se salga por la izquierda
+            velx = 0; // Detener el movimiento horizontal
+        }
+        if (xposicion > canvas.width - 40) {
+            xposicion = canvas.width - 40; // Evitar que el cuadrado se salga por la derecha
+            velx = 0; // Detener el movimiento horizontal
+        }
+        if (yposicion > canvas.height - 40) {
+            yposicion = canvas.height - 40; // Evitar que el cuadrado se salga por la parte inferior
+            vely = 0; // Detener el movimiento vertical
+            velx= 0;
+        }
+    }
+
+
+  //-- 6) Volver a ejecutar update cuando toque
   requestAnimationFrame(update);
 }
 
+
+
 //-- ¡Que empiece la función!
 update();
-
