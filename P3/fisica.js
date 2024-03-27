05ca4409a09d4b167617a6a6570f9271b2f2885d
dperezg2021 <159420331+dperezg2021@users.noreply.github.com>
2024-03-27 14:12:32 +0100

    nsn2

--- P3/fisica.js
+++ P3/fisica.js
@@ -7,7 +7,6 @@ const fireButton = document.getElementById("fire");
 const resetButton = document.getElementById("reset");
 const AnguloValor = document.getElementById("valorangulo");
 const VelocidadValor = document.getElementById("valorvelocidad");
-
 //-- Definir el tamaño del canvas
 canvas.width = 900;
 canvas.height = 400;
@@ -18,7 +17,7 @@ let xposicion = 20;
 let yposicion = canvas.height - 50; // Ajustamos la posición inicial del cuadrado más arriba
 let velx = 0;
 let vely = 0;
-let contadorAciertos = 0;
+let aciertos = 0;
 let flightTime = 0;
 let timerInterval;
 let initialVelocity = 10;
@@ -119,8 +118,8 @@ function dibujarConfeti(x, y) {
 
           // Si el confeti está fuera del canvas, reiniciar su posición
           if (inicialX < 0 || inicialX > canvas.width || inicialY < 0 || inicialY > canvas.height) {
-              inicialX = x + Math.random() * 20 - 10;
-              inicialY = y + Math.random() * 20 - 10;
+            inicialX = x + Math.random() * 20 - 10;
+            inicialY = y + Math.random() * 20 - 10;
           }
 
           // Redibujar el confeti en su nueva posición
@@ -131,17 +130,69 @@ function dibujarConfeti(x, y) {
           ctx.closePath();
       };
 
-      // Llamar a la función para mover el confeti y guardar la identificación del intervalo
-      intervalosConfeti.push(setInterval(moverConfeti, 30));
+    // Llamar a la función para mover el confeti y guardar la identificación del intervalo
+    intervalosConfeti.push(setInterval(moverConfeti, 30));
   }
 
   // Detener los intervalos después de 3 segundos
   setTimeout(() => {
       intervalosConfeti.forEach(intervalo => clearInterval(intervalo));
-  }, 3000);
+  }, 4500);
 }
 
-function dibujarNubes() {
+function dibujarGota(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, 2, 0, Math.PI * 2);
+  ctx.fillStyle = 'blue'; // Color azul para las gotas de lluvia
+  ctx.fill();
+  ctx.closePath();
+}
+
+function dibujarEscenario() {
+  // Limpiar el lienzo
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Dibujar una nube
+  ctx.beginPath();
+  ctx.arc(190, 100, 30, 0, Math.PI * 2);
+  ctx.arc(230, 100, 30, 0, Math.PI * 2);
+  ctx.arc(270, 100, 30, 0, Math.PI * 2);
+  ctx.arc(310, 100, 30, 0, Math.PI * 2);
+  ctx.arc(350, 100, 30, 0, Math.PI * 2);
+  ctx.arc(390, 100, 30, 0, Math.PI * 2);
+  ctx.fillStyle = 'grey';
+  ctx.fill();
+  ctx.closePath();
+
+  // Dibujar otra nube
+  ctx.beginPath();
+  ctx.arc(550, 150, 40, 0, Math.PI * 2);
+  ctx.arc(600, 150, 40, 0, Math.PI * 2);
+  ctx.arc(650, 150, 40, 0, Math.PI * 2);
+  ctx.arc(700, 150, 40, 0, Math.PI * 2);
+  ctx.fillStyle = 'grey';
+  ctx.fill();
+  ctx.closePath();
+
+  // Si el checkbox está marcado, dibujar la lluvia
+  if (document.getElementById('lluviaCheckbox').checked) {
+    // Dibujar gotas de lluvia azules debajo de la primera nube
+    for (let i = 0; i < 30; i++) {
+      let x = Math.random() * 160 + 160;
+      let y = Math.random() * 50 + 120;
+      dibujarGota(x, y);
+    }
+    // Dibujar gotas de lluvia azules debajo de la segunda nube
+    for (let i = 0; i < 30; i++) {
+      let x = Math.random() * 200 + 500;
+      let y = Math.random() * 50 + 170;
+      dibujarGota(x, y);
+    }
+  }
+}
+
+
+function dibujarEscenario() {
   // Dibujar una nube
   ctx.beginPath();
   ctx.arc(190, 100, 30, 0, Math.PI * 2);
@@ -164,6 +215,25 @@ function dibujarNubes() {
   ctx.fillStyle = 'white';
   ctx.fill();
   ctx.closePath();
+
+  //-- Dibujar césped
+  ctx.beginPath();
+  ctx.moveTo(0, canvas.height); // Mover al punto inicial en la esquina inferior izquierda
+  ctx.lineTo(canvas.width, canvas.height); // Dibujar una línea hasta la esquina inferior derecha
+  ctx.strokeStyle = 'green'; // Establecer el color de la línea a verde
+  ctx.lineWidth = 8; // Establecer el ancho de la línea
+  ctx.stroke(); // Dibujar la línea
+  ctx.closePath();
+
+  //-- Dibujar sol
+  ctx.beginPath();
+  ctx.arc(38, 38, 56, 0, 2 * Math.PI); // Círculo exterior
+  ctx.strokeStyle = 'yellow'; // Cambiando el color del borde a amarillo
+  ctx.lineWidth = 2;
+  ctx.fillStyle = 'yellow'; // Cambiando el color de relleno a amarillo
+  ctx.stroke();
+  ctx.fill();
+  ctx.closePath();
 }
 function dibujarContadorAciertos() {
   const gradient = ctx.createRadialGradient(
@@ -182,7 +252,7 @@ function dibujarContadorAciertos() {
 
   ctx.font = '40px KG Happy';
   ctx.fillStyle = gradient;
-  ctx.fillText(`Aciertos: ${contadorAciertos}`, canvas.width - 250, 40);
+  ctx.fillText(`Aciertos: ${aciertos}`, canvas.width - 250, 40);
 }
 
 //-- Evento para el botón de disparo
@@ -213,28 +283,9 @@ function update() {
   //-- 2) Borrar el canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   dibujarContadorAciertos();
-  dibujarNubes()
+  dibujarEscenario()
 
   //-- 3) Dibujar los elementos visibles
-  //-- Dibujar césped
-  ctx.beginPath();
-  ctx.moveTo(0, canvas.height); // Mover al punto inicial en la esquina inferior izquierda
-  ctx.lineTo(canvas.width, canvas.height); // Dibujar una línea hasta la esquina inferior derecha
-  ctx.strokeStyle = 'green'; // Establecer el color de la línea a verde
-  ctx.lineWidth = 5; // Establecer el ancho de la línea
-  ctx.stroke(); // Dibujar la línea
-  ctx.closePath();
-
-  //-- Dibujar sol
-  ctx.beginPath();
-  ctx.arc(38, 38, 56, 0, 2 * Math.PI); // Círculo exterior
-  ctx.strokeStyle = 'yellow'; // Cambiando el color del borde a amarillo
-  ctx.lineWidth = 2;
-  ctx.fillStyle = 'yellow'; // Cambiando el color de relleno a amarillo
-  ctx.stroke();
-  ctx.fill();
-  ctx.closePath();
-
   //-- Dibujar la diana
   ctx.beginPath();
   ctx.arc(nuevaPosicionX, canvas.height - 33, 32, 0, 2 * Math.PI); // Círculo exterior (rojo)
@@ -348,15 +399,17 @@ function update() {
     }
   }
 
-  if (detectarColision()) {
-      resultDisplay.textContent = '¡Acertaste la diana!';
-      stopTimer(); // Detener el contador de tiempo
-      velx = 0; // Detener el movimiento horizontal
-      vely = 0; // Detener el movimiento vertical
-      console.log(contadorAciertos);
-      contadorAciertos++;
-      dibujarConfeti(nuevaPosicionX, canvas.height - 33);
-      return;
+  if (detectarColision() && disparado) {
+
+    aciertos ++;
+    console.log("Aciertos:", aciertos); // Mostrar número de aciertos en consola
+
+    resultDisplay.textContent = '¡Acertaste la diana!';
+    stopTimer(); // Detener el contador de tiempo
+    velx = 0; // Detener el movimiento horizontal
+    vely = 0; // Detener el movimiento vertical
+    dibujarConfeti(nuevaPosicionX, canvas.height - 33);
+    return;
   } else if (!detectarColision() && velx === 0 && vely === 0 && disparado) {
       resultDisplay.textContent = '¡Fallaste!';
       stopTimer(); // Detener el contador de tiempo
@@ -369,6 +422,4 @@ function update() {
 
 //-- ¡Que empiece la función!
 update(); 
-
-
 
